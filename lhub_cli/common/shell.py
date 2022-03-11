d8b0ef363c0c71ea0ca3c38bcb87e556c385cca8
Chad Roberts <jcbroberts@gmail.com>
2022-03-10 16:49:55 -0800

    improvements for the query_yes_no function

--- lhub_cli/common/shell.py
+++ lhub_cli/common/shell.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def query_yes_no(question, default=None):
+def query_yes_no(question, default: (str, bool)=None):
     """Ask a yes/no question via raw_input() and return their answer.
 
     "question" is a string that is presented to the user.
@@ -12,22 +12,30 @@ def query_yes_no(question, default=None):
     The "answer" return value is True for "yes" or False for "no".
     """
     # Borrowed from here: https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
-    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-    if not default:
+    valid_yes = ["yes", "y", "ye"]
+    valid_no = ["no", "n"]
+    if default is not None:
+        if isinstance(default, str):
+            default = default.lower().strip()
+            assert default in valid_yes + valid_no, f"Invalid input for default value: {default}"
+            # Convert to boolean
+            default = default in valid_yes
+
+    if default is None:
         prompt = " [y/n] "
-    elif default is True or default.lower() in ("yes", "y"):
+    elif default is True:
         prompt = " [Y/n] "
-    elif default is False or default.lower() in ("no", "n"):
+    elif default is False:
         prompt = " [y/N] "
     else:
-        raise ValueError("invalid default answer: '%s'" % default)
+        raise ValueError(f"Invalid input for default value: {default}")
 
     while True:
         sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == "":
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
+        choice = input().lower().strip()
+        if choice and choice in valid_yes + valid_no:
+            return choice in valid_yes
+        if not choice and default is not None:
+            return default
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+            sys.stdout.write("Please respond with one of the following: y/n/yes/no\n\n")
