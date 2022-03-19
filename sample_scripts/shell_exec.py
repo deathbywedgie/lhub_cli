942acf6a9419bd1587de488650a0b30339f56a4b
Chad Roberts <jcbroberts@gmail.com>
2022-03-18 19:14:59 -0700

    cleaning up, and taking out some unnecessary fluff

--- sample_scripts/shell_exec.py
+++ sample_scripts/shell_exec.py
@@ -6,6 +6,7 @@ import lhub
 from lhub_cli.shell import Shell
 
 
+# ToDo Add log level option
 def parse_and_validate_args():
     # Range of available args and expected input
     parser = argparse.ArgumentParser(description="Launch the LogicHub remote shell")
@@ -26,28 +27,17 @@ def parse_and_validate_args():
 
     connection.add_argument("--ignore_ssl", action="store_true", help="Ignore SSL warnings")
     connection.add_argument("-t", "--timeout", metavar="<sec>", type=int, default=120, help="HTTP request timeout, except for logon (default: 120)")
-    connection.add_argument("-tl", "--timeout_logon", metavar="<sec>", type=int, default=20, help="Logon timeout (default: 20)")
-
-    # take in the arguments provided by user
-    _args = parser.parse_args()
-
-    lhub.log.log_level = _args.log_level
-    # Logger.default_log_level = _args.log_level
-
-    lhub.LogicHub.http_timeout_login = _args.timeout_logon
-    # lhub.log.debug(f"Logon timeout: {_args.timeout_logon}")
-
-    lhub.LogicHub.http_timeout_default = _args.timeout
-    # lhub.log.debug(f"Other HTTP timeout: {_args.timeout}")
-
-    # if _args.ignore_ssl:
-    #     lhub.log.debug(f"SSL verification disabled")
-    #     # lhub.LogicHub.verify_ssl = False
-
-    return _args
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_and_validate_args()
+    lhub.log.log_level = args.log_level
+
+    lhub.api.LogicHubAPI.http_timeout_default = args.timeout
+    # log = lhub.log.Logger()
+    # log.debug(f"Setting custom HTTP timeout: {args.timeout}")
+
+    # ToDo Add option to provide the connection label up front when the script is invoked
     # Shell.session = LogicHubConnection(instance_alias=args.instance)
     Shell().cmdloop()
