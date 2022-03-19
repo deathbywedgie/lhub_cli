6db21128f2ac54ee9048248ce6d910ca1914fa91
Chad Roberts <jcbroberts@gmail.com>
2022-03-19 03:44:55 -0700

    Completed the conversion to the newer, more secure "~/.logichub/credentials" file for storing passwords and api tokens. At this point there should be no remaining scripts that use the old ConfigFile class for servers.json.

--- lhub_cli/__init__.py
+++ lhub_cli/__init__.py
@@ -1,3 +1,3 @@
-from . import common, config, connection_manager, encryption, features, shell
+from . import common, connection_manager, encryption, features, shell
 from .main import LogicHubCLI
 from .connection_manager import LogicHubConnection
