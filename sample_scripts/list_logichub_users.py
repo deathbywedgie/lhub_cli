724ba34c32c4f131fda4de969257b193788630b7
Chad Roberts <jcbroberts@gmail.com>
2022-03-18 20:47:34 -0700

    Let lhub_cli handle the fancy printing

--- sample_scripts/list_logichub_users.py
+++ sample_scripts/list_logichub_users.py
@@ -1,42 +1,64 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
 import sys
 
 from lhub.log import Logger
-from tabulate import tabulate, tabulate_formats
 
 import lhub_cli
 
-LOG_LEVEL = "INFO"
+DEFAULT_LOG_LEVEL = "INFO"
+DEFAULT_OUTPUT = "json"
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="List all users from one or more LogicHub instances")
 
-    # Inputs required from user
+    # Required inputs
     parser.add_argument("instance_names", nargs="*", help="Names of specific instances from stored config (default: show all)")
 
-    # Optional args:
+    # Optional inputs
     parser.add_argument("--debug", action="store_true", help="Enable debug logging")
 
-    parser.add_argument(
-        "-f", dest="format", type=str, default=None, choices=tabulate_formats, metavar=f"{{{', '.join(tabulate_formats)}}}",
-        help=f"Optional: print output in a table with a specified format (default: None)"
-    )
-    _args = parser.parse_args()
-    if _args.debug:
-        global LOG_LEVEL
-        LOG_LEVEL = "DEBUG"
+    # ToDo Find out if there is a way to bundle this "output" argument group in a way that a script can add it while still having its own args too
+    output = parser.add_argument_group('output')
 
-    return parser.parse_args()
+    output.add_argument(
+        "-f", "--file",
+        type=str,
+        default=None,
+        help="Also write output to a file")
+
+    output.add_argument(
+        "-o", "--output",
+        dest="output_type",
+        type=str,
+        metavar="<OPTION>",
+        default=DEFAULT_OUTPUT,
+        choices=lhub_cli.features.commands.Command.supported_output_types,
+        help=f"Output style (default: {DEFAULT_OUTPUT}). Available output types are: {', '.join(lhub_cli.features.commands.Command.supported_output_types)}")
+
+    # https://github.com/astanin/python-tabulate#table-format
+    output.add_argument(
+        "-t", "--table_format",
+        type=str,
+        metavar="<OPTION>",
+        default=None,
+        choices=lhub_cli.common.output.supported_table_formats,
+        help=f"Table format (ignored if output type is not table). Available formats are: {', '.join(lhub_cli.common.output.supported_table_formats)}"
+    )
+
+    _args = parser.parse_args()
+    _args.log_level = DEFAULT_LOG_LEVEL
+    if _args.debug:
+        _args.log_level = "DEBUG"
+
+    return _args
 
 
 def main():
-    global LOG_LEVEL
     args = get_args()
-    log = Logger(log_level=LOG_LEVEL)
+    log = Logger(log_level=args.log_level)
 
     config = lhub_cli.connection_manager.LogicHubConnection()
     if args.instance_names:
@@ -47,7 +69,7 @@ def main():
     output = []
     for i in instances:
         # If the instance name does not already exist as a saved connection, this will
-        cli = lhub_cli.LogicHubCLI(i, log_level=LOG_LEVEL)
+        cli = lhub_cli.LogicHubCLI(i, log_level=args.log_level)
 
         log.info(f"Connecting to {i}")
         result = cli.session.actions.list_users(hide_inactive=True)
@@ -67,17 +89,16 @@ def main():
             entry["is admin"] = is_admin
             output.append(entry)
 
+    # Sort the results by connection name, hostname, and then username (function lists reverse order)
     output = sorted(output, key=lambda e: (e['username'], e['hostname'], e['connection name']))
-    if args.format:
-        output = tabulate(
-            tabular_data=[x.values() for x in output],
-            headers=output[0].keys(),
-            tablefmt=args.format
-        )
-    else:
-        output = json.dumps(output, indent=2)
 
-    print(output)
+    lhub_cli.common.output.print_fancy_lists(
+        results=output,
+        output_type=args.output_type,
+        table_format=args.table_format,
+        ordered_headers=None,
+        output_file=args.file
+    )
 
 
 if __name__ == "__main__":
