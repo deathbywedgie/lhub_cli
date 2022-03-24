0b1f51e45e94edb7e8b9c2aba8b394f8a75ff263
Chad Roberts <jcbroberts@gmail.com>
2022-03-24 15:42:02 -0700

    general improvements, added helpful comments, and fixed that the args for output type and table format were not being honored

--- sample_scripts/list_logichub_users.py
+++ sample_scripts/list_logichub_users.py
@@ -18,10 +18,14 @@ def get_args():
     parser.add_argument("instance_names", nargs="*", help="Names of specific instances from stored config (default: show all)")
 
     # Optional inputs
+    parser.add_argument("-i", "--inactive", action="store_true", help="Include inactive users in the results")
     parser.add_argument("--debug", action="store_true", help="Enable debug logging")
 
-    # Add standard output arg definitions
-    lhub_cli.common.args.add_script_output_args(parser, default=DEFAULT_OUTPUT)
+    # Add standard output arg definitions:
+    #         "-f", "--file" (Also write output to a file)
+    #         "-o", "--output" (Output style, e.g. table, csv, json, json-pretty)
+    #         "-t", "--table_format" (for output style of table, set a specific table style, such as plain, grid, and jira)
+    lhub_cli.common.args.add_script_output_args(parser, default_output=DEFAULT_OUTPUT)
 
     _args = parser.parse_args()
     _args.log_level = DEFAULT_LOG_LEVEL
@@ -38,34 +42,46 @@ def main():
         instances = args.instance_names
     else:
         config = lhub_cli.connection_manager.LogicHubConnection()
-        instances = config.config.list_configured_instances()
+        instances = config.all_instances
 
     # For all available attributes, set: attributes = "*"
+    # Examples: ["is_admin", "email", "groups", "is_deleted", "is_enabled", "auth_type", "id"]
     attributes = ["email", "is_admin", "groups"]
+
+    show_inactive = args.inactive
+    if args.inactive:
+        for a in ["is_enabled", "is_deleted"]:
+            if a not in attributes:
+                attributes.append(a)
 
     combined_results = []
     for n in progressbar.progressbar(range(len(instances))):
-        _instance = instances[n]
-        cli = lhub_cli.LogicHubCLI(_instance, log_level=args.log_level)
-        cli.session.api.log.debug(f"Connected to {_instance}")
+        cli = lhub_cli.LogicHubCLI(instances[n], log_level=args.log_level)
+        cli.session.api.log.debug(f"Connected to {cli.instance_name}")
         combined_results.extend(
             cli.actions.list_users(
                 attributes=attributes,
                 print_output=False,
-                # return_results=True,
                 show_hostname=True,
-                # hide_inactive=False,
-                # sort_order=[]
+                hide_inactive=show_inactive is False,
             )
         )
 
-    # Set to None in order to leave all results in the default order, in the order of connections as they were requested
-    sort_order = ["connection name"]
+    print_fancy_lists(
+        results=combined_results,
+        output_type=args.output,
+        table_format=args.table_format,
+        output_file=(args.file or None),
 
-    file_only = False
-    if args.file:
-        file_only = True
-    print_fancy_lists(combined_results, sort_order=sort_order, output_file=args.file or None, file_only=file_only)
+        # Enable to provide columns to keep, in order
+        # ordered_headers=[],
+
+        # Set to None in order to leave all results in the default order, in the order of connections as they were requested
+        sort_order=["connection name"],
+
+        # Change to False to always print output even if writing to a file
+        file_only=(True if args.file else False)
+    )
 
 
 if __name__ == "__main__":
