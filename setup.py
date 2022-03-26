7b40a65844847f85ce45d268f9bd5f0f0e5a4b9e
Chad Roberts <jcbroberts@gmail.com>
2022-03-25 17:46:36 -0700

    bundling to release 0.0.6.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6.5'
+VERSION = '0.0.6.6'
 
 setup(
     name="lhub_cli",
@@ -11,7 +11,7 @@ setup(
     long_description="A Python package for interacting with LogicHub via shell commands",
     packages=find_packages(),
     install_requires=[
-        "lhub == 0.1.6.2",
+        "lhub == 0.1.6.3",
         "configobj >= 5.0.6",
         "dataclasses_json >= 0.5.6",
         "rsa >= 4.8",
