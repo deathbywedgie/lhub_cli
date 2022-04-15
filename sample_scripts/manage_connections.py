18cf6be029c25c1a93a3af5c04d6620d72a89770
Chad Roberts <jcbroberts@gmail.com>
2022-04-15 13:29:21 -0700

    * Add support for specifying a custom credential file for organizing credentials when managing many instances or users * Catch lhub_cli exceptions and print friendlier error messages * Loop and redo user prompts if creating a new connection fails

--- sample_scripts/manage_connections.py
+++ sample_scripts/manage_connections.py
@@ -7,6 +7,7 @@ from requests.exceptions import SSLError
 
 from lhub_cli.common.shell import query_yes_no
 from lhub_cli.connection_manager import LhubConfig
+from lhub_cli.exceptions.base import CLIValueError
 
 SHOW_SECURE = False
 VERIFY_SSL = None
@@ -16,6 +17,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="Manage LogicHub CLI connections")
 
     parser.add_argument("instance_label", type=str, nargs="?", help="Label (name) for the connection")
+    parser.add_argument("-cred", "--credentials_file_name", default=None, help="Alternate credentials file name to use (default: \"credentials\")")
 
     parser.add_argument("--show_secure", action="store_true", help="Optional: include passwords and API tokens in output", default=None)
 
@@ -67,46 +69,51 @@ def main():
     args = get_args()
     if args.show_secure:
         SHOW_SECURE = True
-    config = LhubConfig()
+    config = LhubConfig(credentials_file_name=args.credentials_file_name)
 
-    if args.delete:
-        config.delete_connection(args.instance_label)
+    try:
+        if args.delete:
+            config.delete_connection(args.instance_label)
 
-    elif args.create:
-        config.create_instance(
-            instance_label=args.instance_label,
-            server=args.server,
-            auth_type=args.auth_type,
-            api_key=args.api_key,
-            username=args.username,
-            password=args.password,
-            verify_ssl=VERIFY_SSL
-        )
+        elif args.create:
+            print(f"Creating instance: {args.instance_label}")
+            config.create_instance(
+                instance_label=args.instance_label,
+                server=args.server,
+                auth_type=args.auth_type,
+                api_key=args.api_key,
+                username=args.username,
+                password=args.password,
+                verify_ssl=VERIFY_SSL
+            )
 
-    elif args.show_all:
-        instances = config.list_configured_instances()
-        for i in instances:
-            instance_config = config.get_instance(i)
-            print_instance_details(i, instance_config)
+        elif args.show_all:
+            instances = config.list_configured_instances()
+            for i in instances:
+                instance_config = config.get_instance(i)
+                print_instance_details(i, instance_config)
 
-    else:
-        instance_config = config.get_instance(args.instance_label)
-        if not instance_config:
-            first_try = True
-            while first_try or query_yes_no("\nTry again?"):
-                first_try = False
-                # ToDo Catch more errors... what if connection is refused? Times out? Something else entirely?
-                try:
-                    config.create_instance(args.instance_label)
-                except SSLError as err:
-                    print(str(err), file=sys.stderr)
-                except lhub_exceptions.auth.APIAuthFailure:
-                    print("API token authentication failed", file=sys.stderr)
-                except lhub_exceptions.auth.PasswordAuthFailure:
-                    print("Password authentication failed", file=sys.stderr)
-                else:
-                    exit()
-        print_instance_details(args.instance_label, instance_config)
+        else:
+            instance_config = config.get_instance(args.instance_label)
+            if not instance_config:
+                first_try = True
+                while first_try or query_yes_no("\nTry again?"):
+                    first_try = False
+                    # ToDo Catch more errors... what if connection is refused? Times out? Something else entirely?
+                    try:
+                        config.create_instance(args.instance_label)
+                    except SSLError as err:
+                        print(str(err), file=sys.stderr)
+                    except lhub_exceptions.auth.APIAuthFailure:
+                        print("API token authentication failed", file=sys.stderr)
+                    except lhub_exceptions.auth.PasswordAuthFailure:
+                        print("Password authentication failed", file=sys.stderr)
+                    else:
+                        exit()
+            print_instance_details(args.instance_label, instance_config)
+    except CLIValueError as err:
+        print(str(err))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
