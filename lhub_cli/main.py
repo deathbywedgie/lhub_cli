94ab98777a96f7f4c3a1d9b6e197e09ec405ce3b
Chad Roberts <jcbroberts@gmail.com>
2022-04-16 15:43:00 -0700

    Instead of assuming all kwargs go to LogicHubConnection, grab any needed for that class and pass all others to lhub.LogicHub. This will allow a great deal more flexibility in custom scripts so that other features of lhub can be used without having to code directly against lhub instead of lhub_cli.

--- lhub_cli/main.py
+++ lhub_cli/main.py
@@ -10,12 +10,14 @@ class LogicHubCLI:
         #  * Move the logging function out of lhub and into lhub_cli
         #  * Standardize better w/ the "logging" package
         self.instance_name = instance_name
-        self.__config = LogicHubConnection(instance_name, **kwargs)
+        credentials_file_name = kwargs.pop("credentials_file_name", None)
+        self.__config = LogicHubConnection(instance_name, credentials_file_name=credentials_file_name)
         self.session = lhub.LogicHub(
             **self.__config.credentials.to_dict(),
             api_key=self.__config.credentials.api_key,
             password=self.__config.credentials.password,
-            log_level=log_level
+            log_level=log_level,
+            **kwargs
         )
         self.log = self.session.api.log
         self.actions = Actions(
