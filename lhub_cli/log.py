71d140c49c295a3c84fe499a44e02d8b28fc11fd
Chad Roberts <jcbroberts@gmail.com>
2022-04-26 16:45:40 -0700

    Renamed the "log_level" property to "level" to match the logging module standard

--- lhub_cli/log.py
+++ lhub_cli/log.py
@@ -15,31 +15,31 @@ class DefaultLogger:
     default_log_level = "INFO"
 
     def __init__(self, session_prefix=None, log_level=None):
-        self.log_level = log_level if log_level else self.default_log_level
+        self.level = log_level if log_level else self.default_log_level
         self.session_prefix = (session_prefix if session_prefix is not None else generate_log_session_id(self)).strip()
         if self.session_prefix:
             self.session_prefix = f"[{self.session_prefix}] "
 
     @property
-    def log_level(self):
+    def level(self):
         if self.__log_level.lower() not in LOG_LEVELS:
             raise ValueError(f"Invalid log level: {self.__log_level}")
         return self.__log_level
 
-    @log_level.setter
-    def log_level(self, val: str):
+    @level.setter
+    def level(self, val: str):
         if val.lower() not in LOG_LEVELS:
             raise ValueError(f"Invalid log level: {val}")
         self.__log_level = val.upper()
 
     # temporary to make this fit logging package as closely as possible until we phase this out
     def setLevel(self, level: str):
-        self.log_level = level
+        self.level = level
 
     def __print(self, level, msg):
         level_num = _LOG_LEVEL_MAP[level.lower()]
         output_file = sys.stdout if level_num >= 5 else sys.stderr
-        current_level_num = _LOG_LEVEL_MAP[self.log_level.lower()]
+        current_level_num = _LOG_LEVEL_MAP[self.level.lower()]
         if current_level_num >= level_num:
             print(f"{self.session_prefix}[{level.upper()}] {msg}", file=output_file)
         if level_num == 0:
