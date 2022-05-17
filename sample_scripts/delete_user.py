c36c0e3a3719098c41da85cbdc0350988ec2aff2
Chad Roberts <jcbroberts@gmail.com>
2022-05-17 15:22:56 -0700

    simplify script with recent lhub_cli enhancements

--- sample_scripts/delete_user.py
+++ sample_scripts/delete_user.py
@@ -2,45 +2,24 @@
 
 import argparse
 import json
-import sys
-
-from lhub.exceptions.base import LhBaseException
-from requests.exceptions import HTTPError
-
 import lhub_cli
 
 
 # available args and expected input
 def get_args():
     parser = argparse.ArgumentParser(description="Delete a LogicHub user")
-
-    # Inputs required from user
     parser.add_argument("instance_name", help="Nickname of the instance from stored config")
     parser.add_argument("user", help="Username to delete")
-
-    # Optional args:
-    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
-
-    return parser.parse_args()
+    return lhub_cli.common.args.finish_parser_args_with_logger_only(parser)
 
 
-args = get_args()
-
-# If the instance name does not already exist as a saved connection, this will assist the user in saving a new one.
-cli = lhub_cli.LogicHubCLI(
-    instance_name=args.instance_name,
-
-    # If the --debug option was passed by the user, set log level to debug, otherwise leave it default
-    log_level="DEBUG" if args.debug else None
-)
-
-try:
+def main():
+    args = get_args()
+    # If the instance name does not already exist as a saved connection, this will assist the user in saving a new one.
+    cli = lhub_cli.LogicHubCLI(instance_name=args.instance_name)
     results = cli.session.actions.delete_user_by_name(usernames=args.user)
-except HTTPError as e:
-    print(f"Failed with exception:\n{repr(e)}\n\nLast server response:\n{cli.session.api.last_response_text}", file=sys.stderr)
-    sys.exit(1)
-except LhBaseException as e:
-    print(f"Failed with exception:\n{repr(e)}", file=sys.stderr)
-    sys.exit(1)
+    print(json.dumps(results, indent=2))
 
-print(json.dumps(results, indent=2))
+
+if __name__ == "__main__":
+    lhub_cli.common.shell.main_script_wrapper(main)
