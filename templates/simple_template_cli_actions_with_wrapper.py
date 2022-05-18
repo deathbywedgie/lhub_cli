ab412f23cfb0d7663348a42d70b41a5135ef3818
Chad Roberts <jcbroberts@gmail.com>
2022-05-17 21:00:11 -0700

    new script template

---
+++ templates/simple_template_cli_actions_with_wrapper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+import lhub_cli
+import json
+
+
+# available args and expected input
+def get_args():
+    parser = argparse.ArgumentParser(description="Simple script for testing API calls")
+
+    # Inputs required from user
+    parser.add_argument("instance_name", help="Nickname of the instance from stored config")
+
+    # Optional args:
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+
+    return parser.parse_args()
+
+
+args = get_args()
+
+
+def main():
+    # If the instance name does not already exist as a saved connection, this will assist the user in saving a new one.
+    cli = lhub_cli.LogicHubCLI(
+        instance_name=args.instance_name,
+
+        # If the --debug option was passed by the user, set log level to debug, otherwise leave it default
+        log_level="DEBUG" if args.debug else None
+    )
+
+    # Choose the CLI action to execute. Below is an example for
+    result = cli.actions.list_users()
+    print(json.dumps(result))
+
+
+if __name__ == "__main__":
+    lhub_cli.common.shell.main_script_wrapper(main)
