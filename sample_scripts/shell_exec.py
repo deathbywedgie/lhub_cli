7aec412d8a2d8f2ae6064a0a6dd46deef3705f34
Chad Roberts <jcbroberts@gmail.com>
2022-05-17 20:52:34 -0700

    simplify scripts with recent lhub_cli enhancements

--- sample_scripts/shell_exec.py
+++ sample_scripts/shell_exec.py
@@ -2,41 +2,32 @@
 
 import argparse
 import lhub
-from logging import _nameToLevel as AllowedLogLevels
 
-from lhub_cli.shell import Shell
+import lhub_cli
 
 
-# ToDo Add log level option
 def parse_and_validate_args():
-    # Range of available args and expected input
-    parser = argparse.ArgumentParser(description="Launch the LogicHub remote shell")
+    _parser = argparse.ArgumentParser(description="Launch the LogicHub remote shell")
+    _parser.add_argument("instance", metavar="INSTANCE", type=str, nargs="?", default=None, help="Name/label of the instance to which you wish to connect")
 
-    # Required input
-    parser.add_argument("instance", metavar="INSTANCE", type=str, nargs="?", default=None, help="Name/label of the instance to which you wish to connect")
+    connection = _parser.add_argument_group('connection')
+    connection.add_argument("--ignore_ssl", action="store_true", help="Ignore SSL warnings")
+    connection.add_argument("-ti", "--timeout", metavar="<sec>", type=int, default=120, help="HTTP request timeout, except for logon (default: 120)")
 
-    parser.add_argument(
-        "-log", "--log_level",
-        metavar="<level>",
-        type=str,
-        default="info",
-        choices=AllowedLogLevels,
-        help="Log level (default: info)"
+    return lhub_cli.common.args.build_args_and_logger(
+        parser=_parser,
+        include_logging_args=True,
+        # default_log_level="INFO",
     )
 
-    connection = parser.add_argument_group('connection')
 
-    connection.add_argument("--ignore_ssl", action="store_true", help="Ignore SSL warnings")
-    connection.add_argument("-t", "--timeout", metavar="<sec>", type=int, default=120, help="HTTP request timeout, except for logon (default: 120)")
-    return parser.parse_args()
+args, _ = parse_and_validate_args()
 
 
-if __name__ == '__main__':
-    args = parse_and_validate_args()
-
+def main():
     lhub.api.LogicHubAPI.http_timeout_default = args.timeout
-    # log = lhub.log.Logger()
-    # log.debug(f"Setting custom HTTP timeout: {args.timeout}")
+    lhub_cli.shell.Shell(log_level=args.LOG_LEVEL).cmdloop()
 
-    # ToDo Add option to provide the connection label up front when the script is invoked
-    Shell(log_level=args.log_level).cmdloop()
+
+if __name__ == "__main__":
+    lhub_cli.common.shell.main_script_wrapper(main)
