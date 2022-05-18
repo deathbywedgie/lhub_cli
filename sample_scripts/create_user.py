9bbf8a6590f6aca3b083a3d037f1b75a796ec902
Chad Roberts <jcbroberts@gmail.com>
2022-05-17 18:59:54 -0700

    simplify scripts with recent lhub_cli enhancements

--- sample_scripts/create_user.py
+++ sample_scripts/create_user.py
@@ -7,17 +7,21 @@ import lhub_cli
 
 # available args and expected input
 def get_args():
-    parser = argparse.ArgumentParser(description="Create a new LogicHub user")
-    parser.add_argument("instance_name", help="Nickname of the instance from stored config")
-    parser.add_argument("user", help="Username for the new user")
-    parser.add_argument("email", help="Email address for the new user")
-    return lhub_cli.common.args.finish_parser_args_with_logger_only(parser)
+    _parser = argparse.ArgumentParser(description="Create a new LogicHub user")
+    _parser.add_argument("instance_name", help="Nickname of the instance from stored config")
+    _parser.add_argument("user", help="Username for the new user")
+    _parser.add_argument("email", help="Email address for the new user")
+    return lhub_cli.common.args.build_args_and_logger(
+        parser=_parser,
+        include_logging_args=True
+    )
 
 
 def main():
-    args = get_args()
+    args, logger = get_args()
     # If the instance name does not already exist as a saved connection, this will assist the user in saving a new one.
     cli = lhub_cli.LogicHubCLI(instance_name=args.instance_name)
+
     results = cli.actions.create_user(
         username=args.user,
         email=args.email,
--- sample_scripts/delete_user.py
+++ sample_scripts/delete_user.py
@@ -7,17 +7,21 @@ import lhub_cli
 
 # available args and expected input
 def get_args():
-    parser = argparse.ArgumentParser(description="Delete a LogicHub user")
-    parser.add_argument("instance_name", help="Nickname of the instance from stored config")
-    parser.add_argument("user", help="Username to delete")
-    return lhub_cli.common.args.finish_parser_args_with_logger_only(parser)
+    _parser = argparse.ArgumentParser(description="Delete a LogicHub user")
+    _parser.add_argument("instance_name", help="Nickname of the instance from stored config")
+    _parser.add_argument("user", help="Username to delete")
+    return lhub_cli.common.args.build_args_and_logger(
+        parser=_parser,
+        include_logging_args=True
+    )
 
 
 def main():
-    args = get_args()
+    args, logger = get_args()
     # If the instance name does not already exist as a saved connection, this will assist the user in saving a new one.
     cli = lhub_cli.LogicHubCLI(instance_name=args.instance_name)
-    results = cli.session.actions.delete_user_by_name(usernames=args.user)
+
+    results = cli.actions.delete_user_by_name(username=args.user)
     print(json.dumps(results, indent=2))
 
 
