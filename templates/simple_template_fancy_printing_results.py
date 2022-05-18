d43785d2b6a2951bee4558b963125389c47712d8
Chad Roberts <jcbroberts@gmail.com>
2022-05-17 20:59:20 -0700

    simplify scripts with recent lhub_cli enhancements

--- templates/simple_template_fancy_printing_results.py
+++ templates/simple_template_fancy_printing_results.py
@@ -5,31 +5,21 @@ import argparse
 import lhub_cli
 from lhub_cli.common.output import print_fancy_lists
 
-SCRIPT_DESCRIPTION = "Simple script for testing API calls"
 
-
-# available args and expected input
 def get_args():
-    parser = argparse.ArgumentParser(description=SCRIPT_DESCRIPTION)
+    _parser = argparse.ArgumentParser(description="Simple script for testing API calls")
+    _parser.add_argument("instance_name", help="Nickname of the instance from stored config")
 
-    # Inputs required from user
-    parser.add_argument("instance_name", help="Nickname of the instance from stored config")
-
-    # Add standard output arg definitions
-    lhub_cli.common.args.add_script_output_args(parser)
-
-    return parser.parse_args()
+    return lhub_cli.common.args.build_args_and_logger(
+        parser=_parser,
+        include_list_output_args=True
+    )
 
 
-args = get_args()
+args, _ = get_args()
 
 # If the instance name does not already exist as a saved connection, this will assist the user in saving a new one.
-cli = lhub_cli.LogicHubCLI(
-    instance_name=args.instance_name,
-
-    # If the --debug option was passed by the user, set log level to debug, otherwise leave it default
-    log_level="DEBUG" if args.debug else None
-)
+cli = lhub_cli.LogicHubCLI(instance_name=args.instance_name)
 
 results = cli.session.actions.list_users(hide_inactive=True)
 # dict_keys(['userId', 'name', 'groups', 'role', 'authenticationType', 'preferences', 'isEnabled', 'isDeleted'])
