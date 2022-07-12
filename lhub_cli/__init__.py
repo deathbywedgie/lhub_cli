4273a92315b76a45ea95eb3f0da43f1882673fc5
Chad Roberts <jcbroberts@gmail.com>
2022-07-11 17:10:02 -0700

    New LogicHubBulkCLI class w/ preliminary support for asyncio to help w/ automating against multiple instances

--- lhub_cli/__init__.py
+++ lhub_cli/__init__.py
@@ -2,4 +2,4 @@ from . import log
 from . import common, connection_manager, encryption, exceptions, features, shell
 from .common.config import list_credential_files
 from .connection_manager import LogicHubConnection
-from .main import LogicHubCLI, list_all_instances
+from .main import LogicHubCLI, LogicHubBulkCLI, list_all_instances
