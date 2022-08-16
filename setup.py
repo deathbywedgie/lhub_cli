8477e38232b6e249a54f53d54669295ceda7951c
Chad Roberts <jcbroberts@gmail.com>
2022-08-16 14:54:41 -0700

    bug fix: failed to include structlog in requirements

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "requests",
         "tabulate",
         "colorama",
+        "structlog",
         "progressbar2",  # Only used in sample scripts for now, and imported not as "progressbar2" but just "progressbar"
     ],
     keywords=["python", "lhub", "LogicHub", "api", "security", "cli"],
